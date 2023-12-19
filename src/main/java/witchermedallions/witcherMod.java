130c74204b1ebcdeb7c87f95adf190febda0efa6
angeldgr <chechedano@gmail.com>
2023-12-19 03:35:13 -0600

    AAAAA

--- src/main/java/witchermedallions/witcherMod.java
+++ src/main/java/witchermedallions/witcherMod.java
@@ -15,7 +15,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.bernie.geckolib3.GeckoLib;
 
-//import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
 import witchermedallions.items.ModGroups;
 import witchermedallions.items.ModItems;
