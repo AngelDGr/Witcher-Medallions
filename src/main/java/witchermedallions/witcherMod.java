32e3c91542db5e515a7684c693f8debefde1dd69
angeldgr <chechedano@gmail.com>
2023-12-19 03:53:20 -0600

    1.0.0 Release

--- src/main/java/witchermedallions/witcherMod.java
+++ src/main/java/witchermedallions/witcherMod.java
@@ -1,7 +1,7 @@
 package witchermedallions;
 
 import net.fabricmc.api.ModInitializer;
-//Patataaaaaaaaa
+
 //Minecraft Libraries
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.entity.model.EntityModel;
